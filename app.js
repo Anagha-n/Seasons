577938526884e1f76389eb757dc587dbc29774ba
Anagha-n <67535240+Anagha-n@users.noreply.github.com>
2020-07-19 13:45:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+document.getElementById("time").innerHTML = Date()
+
+function copy() {
+    alert("You can't Copy this! It's my work")
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hey!! It's Summer 😎")
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hey!! It's Winter 🥶")
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hey!! It's Autumn 🍂")
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundRepeat = "no repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hey!! It's Spring 🌺")
+}
